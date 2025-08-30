7cfedaa7a6344d5a17367c8c041aa651af0eea7f
mufaddalratlamwala-iet <22btc043@ietdavv.edu.in>
2025-08-30 21:31:52 +0530

    restructured backend

--- server/routes/company.js
+++ server/routes/company.js
@@ -1,18 +1,9 @@
 import express from "express";
-import Company from "../models/companySchema.js";
 
 const router = express.Router();
+import { registerCompany } from "../controllers/company.js";
 
 // POST /register - Register a new company
-router.post("/register", async (req, res) => {
-  try {
-        const { name, industryType, businessEmail, contactNos, companySize, fullAddress, workForceType } = req.body;
-    const company = new Company({ name, industryType, businessEmail, contactNos, companySize, fullAddress, workForceType });
-    await company.save();
-    res.status(201).json({ message: "Company registered successfully", company });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/register", registerCompany);
 
 export default router;
