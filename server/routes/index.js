f5a341c5dccf2517100f32b6a8ca3e4692f6f630
mufaddalratlamwala-iet <22btc043@ietdavv.edu.in>
2025-08-30 21:17:51 +0530

    company registration

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
-
+import companyRoutes from './company.js'
 const router = express.Router();
 
 // Example route
 router.get('/', (req, res) => {
   res.json({ message: 'Crewzy API is running' });
 });
+
+router.use('/company', companyRoutes);
 
 export default router;
